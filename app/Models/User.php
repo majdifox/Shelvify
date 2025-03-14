cdc740b194fd5889ab22801d4b212fddb81151bb
majmehdi <majmehdi20@gmail.com>
2025-03-14 22:37:43 +0000

    finishing the CRUD

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,11 @@ class User extends Authenticatable
      * @return array<string, string>
      */
 
+     protected function casts(): array
+     {
+         return [
+             'email_verified_at' => 'datetime',
+             'password' => 'hashed',
+         ];
+     }
 }
