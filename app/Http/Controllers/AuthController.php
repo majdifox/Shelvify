acf3dd536fce846fbfa89b061faf161e28c052ad
majmehdi <majmehdi20@gmail.com>
2025-03-14 22:28:20 +0000

    Fixing the AuthController adding the login register and logout methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,5 +10,59 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
-   
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'role' => 'required|in:client,admin',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+        ]);
+
+        return response()->json(['message' => 'User successfully registered'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        if($user->role === 'admin'){
+            return response()->json([
+                'message' => 'Login successful as Admin',
+                'token' => $token,
+            ]);
+        }
+        else{
+            return response()->json([
+                'message' => 'Login successful as Client',
+                'token' => $token,
+            ]);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+    
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
 }
