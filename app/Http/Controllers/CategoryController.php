6141529ace6e6f12889ceef57ed67f3ab6b44eb1
majmehdi <majmehdi20@gmail.com>
2025-03-14 22:29:46 +0000

    adding the update and the destroy fro the categoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -34,3 +34,31 @@ class CategoryController extends Controller
         return response()->json($category, 201);
     }
 
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+        if (!$category) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+
+        $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $category->update($request->all());
+        return response()->json($category);
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+        if (!$category) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+
+        $category->delete();
+        return response()->json(['message' => 'Category removed']);
+    }
+
+}
